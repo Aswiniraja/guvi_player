c27cd719bf22de93083bc3868b8df294a6a56f6b
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-28 15:22:58 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -2,7 +2,7 @@ public static void main(String[] args)
 	{
 		Scanner sc=new Scanner(System.in);
 		
-		int n=sc.nextInt();
+		int n=Math.abs(sc.nextInt());
 		int fact=1;
 		for(int i=1;i<=n;i++)
 		{
