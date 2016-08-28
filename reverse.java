a7ecce1dc1d50e6990f4be21e9075b4214697b79
Aswiniraja <aswinirajaeee@gmail.com>
2016-08-28 15:20:15 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		
+		s=new StringBuffer(s).reverse().toString();
+		System.out.println(s);
+
+	}
